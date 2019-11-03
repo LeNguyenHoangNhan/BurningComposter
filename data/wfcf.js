b5503581512ee0337095b079977ebf31db38e023
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-11-03 17:28:47 +0700

    Fix error in wfcf.js

--- data/wfcf.js
+++ data/wfcf.js
@@ -1,7 +1,8 @@
-function updateWiFi(): String {
+function updateWiFi() {
     var ssid = document.getElementById("ssid").innerText;
     var pass = document.getElementById("pass").innerText;
     console.log("SSID: " + ssid);
     console.log("PASS: " + pass);
+    var http = new XMLHttpRequest;
     return
 }
