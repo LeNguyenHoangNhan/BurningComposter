276c915cb06c00ce8b065d198b9f1b828f4824e4
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-11-04 00:41:55 +0700

    Finish WiFi Config Functionality

--- data/wfcf.js
+++ data/wfcf.js
@@ -1,8 +1,27 @@
-function updateWiFi() {
-    var ssid = document.getElementById("ssid").innerText;
-    var pass = document.getElementById("pass").innerText;
+function sendConfig() {
+    var ssid_elem = document.getElementById("ssid");
+    var ssid = ssid_elem.value;
+    var pass_elem = document.getElementById("pass");
+    var pass = pass_elem.value;
     console.log("SSID: " + ssid);
     console.log("PASS: " + pass);
-    var http = new XMLHttpRequest;
+    if (ssid == "") {
+        console.log("SSID is empty");
+        ssid_elem.focus();
+    }  else if (pass == "") {
+        console.log("PASS is emty");
+        pass_elem.focus();
+    } else if (pass.length < 8) {
+        console.log("PASS too short");
+        pass_elem.focus();
+    }
+    var http = new XMLHttpRequest();
+    const url = "/configWifi";
+    var json_data = JSON.stringify({"ssid" : ssid, "pass" : pass});
+    console.log("JSON will be send to server: " + json_data);
+    http.open("POST", "/wificonfig", true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send(json_data);
+
     return
 }
