afbc5340899836ff06eb2e0e4450f999ff89b062
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-11-10 19:26:11 +0700

    C++ Array size template

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,8 @@ int ReadJsonFile(String &target, const char *path, const char *field) {
 /** Implement JSON reader function using stack allocated memory
  *  !!CAUTION!! USE WITH CARE, NOT TESTED MUCH
  */
-int ReadJsonFile(char *target, const char *path, const char *field, size_t target_size) {
+template <typename T, size_t size>
+int ReadJsonFile(T (&target)[size], const char *path, const char *field) {
     Serial.printf("Reading field:%s from file: %s\n", field, path);
     File file = SPIFFS.open("/wfcfg.json", "r");
     ArduinoJson::StaticJsonDocument<1024> doc;
@@ -87,9 +88,9 @@ int ReadJsonFile(char *target, const char *path, const char *field, size_t targe
     }
     const char *field_value = doc[field];
     Serial.printf("Field value: %s\n", field_value);
-    //strcpy(target, field_value); // Consider replace strcpy with snprinf
-    
-    snprintf(target, target_size, "%s", field_value);
+    // strcpy(target, field_value); // Consider replace strcpy with snprinf
+
+    snprintf(target, size, "%s", field_value);
 
     Serial.printf("Target value: %s\n", target);
     file.close();
@@ -140,27 +141,27 @@ void setup() {
 
         Serial.println("Reading Config File");
 #if USE_STATIC_MEMORY
-        if (ReadJsonFile(AP_SSID_char, "/wfcfg.json", "AP_SSID", 64)) {
+        if (ReadJsonFile(AP_SSID_char, "/wfcfg.json", "AP_SSID")) {
             lcd_err_clr_pr(lcd, LCD_ERR_FAILED_READ_CONFIG_AP_SSID);
             delay(1000);
             snprintf(AP_SSID_char, sizeof(AP_SSID_char), "WiFi_Config");
         }
-        if (ReadJsonFile(AP_PASS_char, "/wfcfg.json", "AP_PASS", 64)) {
+        if (ReadJsonFile(AP_PASS_char, "/wfcfg.json", "AP_PASS")) {
             lcd_err_clr_pr(lcd, LCD_ERR_FAILED_READ_CONFIG_AP_PASS);
             delay(1000);
             snprintf(AP_PASS_char, sizeof(AP_PASS_char), "12345678");
         }
-        if (ReadJsonFile(STA_SSID_char, "/wfcfg.json", "STA_SSID", 64)) {
+        if (ReadJsonFile(STA_SSID_char, "/wfcfg.json", "STA_SSID")) {
             lcd_err_clr_pr(lcd, LCD_ERR_FAILED_READ_CONFIG_STA_SSID);
             delay(1000);
             snprintf(STA_SSID_char, sizeof(STA_SSID_char), "hNiP");
         }
-        if (ReadJsonFile(STA_PASS_char, "/wfcfg.json", "STA_PASS", 64)) {
+        if (ReadJsonFile(STA_PASS_char, "/wfcfg.json", "STA_PASS")) {
             lcd_err_clr_pr(lcd, LCD_ERR_FAILED_READ_CONFIG_STA_PASS);
             delay(1000);
             snprintf(STA_PASS_char, sizeof(STA_PASS_char), "LunarQueen12273");
         }
-        if (ReadJsonFile(UUID_char, "/wfcfg.json", "UUID", 64)) {
+        if (ReadJsonFile(UUID_char, "/wfcfg.json", "UUID")) {
             lcd_err_clr_pr(lcd, LCD_ERR_FAILED_READ_CONFIG_STA_PASS);
             delay(1000);
             snprintf(UUID_char, sizeof(UUID_char), "abcdefgh");
@@ -295,7 +296,6 @@ void setup() {
 void loop() {
 #define USE_LOOP
 #ifdef USE_LOOP
-
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.printf("Humd: %.2f", humd);
