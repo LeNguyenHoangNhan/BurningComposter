05874fa0bacfbb288f1130bdbca2e629a2b08a3a
LeNguyenHoangNhan <public.hoangnhanlenguyen@gmail.com>
2019-11-14 11:47:14 +0700

    Add license notices in source code

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -1,13 +1,31 @@
+
+/*
+    This file is part of Burning Composter - A compost monitoring device
+    based on ESP32 and Arduino Core.
+
+    Burning Composter  is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Burning Composter  is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "lcd.hpp"
 
 #ifdef USE_TEMPLATE
 template <typename T>
 void lcd_err_clr_pr(LiquidCrystal_I2C &lcd, T content) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Error:");
-  lcd.setCursor(0, 1);
-  lcd.print(content);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Error:");
+    lcd.setCursor(0, 1);
+    lcd.print(content);
 }
 template <typename T>
 void lcd_clr_pr(LiquidCrystal_I2C &lcd, T content) {}
@@ -15,18 +33,18 @@ void lcd_clr_pr(LiquidCrystal_I2C &lcd, T content) {}
 
 #ifndef USE_TEMPLATE
 void lcd_err_clr_pr(LiquidCrystal_I2C &lcd, const char *content) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Error");
-  lcd.setCursor(0, 1);
-  lcd.print(content);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Error");
+    lcd.setCursor(0, 1);
+    lcd.print(content);
 }
 void lcd_err_clr_pr(LiquidCrystal_I2C &lcd, const String &content) {
-  lcd_err_clr_pr(lcd, content.c_str());
+    lcd_err_clr_pr(lcd, content.c_str());
 }
 void lcd_err_clr_pr(LiquidCrystal_I2C &lcd, int err_code) {
-  char buff[4];
-  snprintf(buff, 3, "%d", err_code);
-  lcd_err_clr_pr(lcd, buff);
+    char buff[4];
+    snprintf(buff, 3, "%d", err_code);
+    lcd_err_clr_pr(lcd, buff);
 }
 #endif /* USE_TEMPLATE */
