d6caa2d58b770e8aeec34f0d216f2333ea5c0ed9
Lê Nguyễn Hoàng Nhân <public.hoangnhanlenguyen@gmail.com>
2019-12-01 07:47:04 +0700

    Update lcd.cpp

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -2,7 +2,9 @@
 /*
     This file is part of Burning Composter - A compost monitoring device
     based on ESP32 and Arduino Core.
-
+        
+        Copyright (c) 2019 Le Nguyen Hoang Nhan
+        
     Burning Composter  is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
